abe5f9ddccbd00eda1acf03f4f9aa2141c409ce9
Beaskyy <afeez.akintobi@yahoo.com>
2024-08-31 12:14:00 +0100

    authentication

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -8,9 +8,13 @@ import { SignUpCard } from "./sign-up-card";
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
   return (
-    <div className="h-full flex justify-center items-center bg-[#5C3B58]">
+    <div className="h-screen flex justify-center items-center bg-[#5C3B58]">
       <div className="md:h:auto md:w-[420px]">
-        {state === "signIn" ? <SignInCard /> : <SignUpCard />}
+        {state === "signIn" ? (
+          <SignInCard setState={setState} />
+        ) : (
+          <SignUpCard setState={setState} />
+        )}
       </div>
     </div>
   );
