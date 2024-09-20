ee06bd69a623fc2159722de8dd80cadd5698f17d
Beaskyy <afeez.akintobi@yahoo.com>
2024-09-20 23:12:01 +0100

    workspace header

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -1,7 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { ChevronDown } from "lucide-react";
 
-export const WorkspaceHeader = () => {
-  return (
-    <div>WorkspaceHeader</div>
-  )
+interface WorkspaceHeaderProps {
+  workspace: Doc<"workspaces">;
 }
 
+export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="transparent"
+            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+            size="sm"
+          >
+            <span className="truncate">{workspace.name}</span>
+            <ChevronDown className="size-4 ml-1 shrink-0" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" align="start" className="w-64">
+          <DropdownMenuItem className="cursor-pointer capitalize">
+            <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold rounded-md text-xl flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
