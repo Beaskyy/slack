fab900a565aba2e9e51d16f1f33706d1c92c6343
Beaskyy <afeez.akintobi@yahoo.com>
2024-09-21 10:06:50 +0100

    workspace

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -8,13 +8,18 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Doc } from "../../../../convex/_generated/dataModel";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
+import { Hint } from "@/components/hint";
 
 interface WorkspaceHeaderProps {
   workspace: Doc<"workspaces">;
+  isAdmin: boolean;
 }
 
-export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
+export const WorkspaceHeader = ({
+  workspace,
+  isAdmin,
+}: WorkspaceHeaderProps) => {
   return (
     <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
       <DropdownMenu>
@@ -33,9 +38,42 @@ export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
             <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold rounded-md text-xl flex items-center justify-center mr-2">
               {workspace.name.charAt(0).toUpperCase()}
             </div>
+            <div className="flex flex-col items-start">
+              <p className="font-bold">{workspace.name}</p>
+              <p className="text-xs text-muted-foreground">Active workspace</p>
+            </div>
           </DropdownMenuItem>
+          {isAdmin && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className=" cursor-pointer py-2"
+                onClick={() => {}}
+              >
+                Invite people to {workspace.name}
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className=" cursor-pointer py-2"
+                onClick={() => {}}
+              >
+                Preferences
+              </DropdownMenuItem>
+            </>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
+      <div className="flex items-center gap-0.5">
+      <Hint label="Filter conversation" side="bottom">
+        <Button variant="transparent" size="iconSm">
+          <ListFilter className="size-4" />
+        </Button>
+        </Hint>
+        <Hint label="New message" side="bottom">
+        <Button variant="transparent" size="iconSm">
+          <SquarePen className="size-4" />
+        </Button>
+        </Hint>
+      </div>
     </div>
   );
 };
