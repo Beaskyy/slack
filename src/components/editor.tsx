d4959df10c23fc9f5f743d4640b8031bda47ad6b
Beaskyy <afeez.akintobi@yahoo.com>
2024-09-26 23:03:24 +0100

    quill editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,13 +1,39 @@
 import Quill, { QuillOptions } from "quill";
 
 import "quill/dist/quill.snow.css";
-import { useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import { PiTextAa } from "react-icons/pi";
 import { ImageIcon, Smile } from "lucide-react";
 import { MdSend } from "react-icons/md";
+import { Hint } from "./hint";
+// import { Delta, Op } from "quill/core";
 
-const Editor = () => {
+type EditorValue = {
+  image: File | null;
+  body: string;
+};
+
+interface EditorProps {
+  onSubmit: ({ image, body }: EditorValue) => void;
+  onCancel?: () => void;
+  placholder?: string;
+  // defaultValue?: Delta | Op[];
+  defaultValue?: any;
+  disabled?: boolean;
+  innerRef?: MutableRefObject<Quill | null>;
+  variant?: "create" | "update";
+}
+
+const Editor = ({
+  onCancel,
+  onSubmit,
+  placholder = "Write something...",
+  defaultValue,
+  disabled = false  ,
+  innerRef,
+  variant = "create",
+}: EditorProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -36,40 +62,73 @@ const Editor = () => {
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
         <div className="flex px-2 pb-2 z-[5]">
-          <Button
-            disabled={false}
-            variant="ghost"
-            size="iconSm"
-            onClick={() => {}}
-          >
-            <PiTextAa className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            variant="ghost"
-            size="iconSm"
-            onClick={() => {}}
-          >
-            <Smile className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            variant="ghost"
-            size="iconSm"
-            onClick={() => {}}
-          >
-            <ImageIcon className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            onClick={() => {}}
-            size="iconSm"
-            className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
-          >
-            <MdSend className="size-4" />
-          </Button>
+          <Hint label="Hide formatting">
+            <Button
+              disabled={false}
+              variant="ghost"
+              size="iconSm"
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4" />
+            </Button>
+          </Hint>
+          <Hint label="Emoji">
+            <Button
+              disabled={false}
+              variant="ghost"
+              size="iconSm"
+              onClick={() => {}}
+            >
+              <Smile className="size-4" />
+            </Button>
+          </Hint>
+          {variant === "create" && (
+            <Hint label="Image">
+              <Button
+                disabled={false}
+                variant="ghost"
+                size="iconSm"
+                onClick={() => {}}
+              >
+                <ImageIcon className="size-4" />
+              </Button>
+            </Hint>
+          )}
+          {variant === "update" && (
+            <div className="ml-auto flex items-center gap-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => {}}
+                disabled={false}
+              >
+                Cancel
+              </Button>
+              <Button
+                className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+                size="sm"
+                onClick={() => {}}
+                disabled={false}
+              >
+                Save
+              </Button>
+            </div>
+          )}
+          {variant === "create" && (
+            <Button
+              disabled={false}
+              onClick={() => {}}
+              size="iconSm"
+              className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+            >
+              <MdSend className="size-4" />
+            </Button>
+          )}
         </div>
       </div>
+      <p className="p-2 text-[10px] text-muted-foreground flex justify-end">
+        <strong>Shift + Return</strong> to add a new line
+      </p>
     </div>
   );
 };
