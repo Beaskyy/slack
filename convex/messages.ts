dc5694956d3f18743d046b61d65b9f414fa9bec6
Beaskyy <afeez.akintobi@yahoo.com>
2024-11-03 19:44:27 +0100

    load messages

--- convex/messages.ts
+++ convex/messages.ts
@@ -3,6 +3,57 @@ import { QueryCtx, mutation } from "./_generated/server";
 import { auth } from "./auth";
 import { Id } from "./_generated/dataModel";
 
+const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
+  const messages = await ctx.db
+    .query("messages")
+    .withIndex("by_parent_message_id", (q) =>
+      q.eq("parentMessageId", messageId)
+    )
+    .collect();
+
+  if (messages.length === 0) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessage = messages[messages.length - 1];
+  const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+
+  if (!lastMessageMember) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessageUser = await populateUser(ctx, lastMessageMember.userId)
+
+  return {
+    count: messages.length,
+    image: lastMessageUser?.image,
+    timestamp: lastMessage._creationTime,
+  }
+};
+
+const populateReactions = (ctx: QueryCtx, messageId: Id<"messages">) => {
+  return ctx.db
+    .query("reactions")
+    .withIndex("by_message_id", (q) => q.eq("messageId", messageId))
+    .collect();
+};
+
+const populateUser = (ctx: QueryCtx, userId: Id<"users">) => {
+  return ctx.db.get(userId);
+};
+
+const populateMember = (ctx: QueryCtx, memberId: Id<"members">) => {
+  return ctx.db.get(memberId);
+};
+
 const getMember = async (
   ctx: QueryCtx,
   workspaceId: Id<"workspaces">,
@@ -38,19 +89,18 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
 
-    let _conversation_id = args.conversationId
+    let _conversation_id = args.conversationId;
 
     // Only possible if we are replying in a thread in 1:1 conversation
-    if(!args.conversationId && !args.channelId && !args.parentMessageId) {
-      const parentMessage = await ctx.db.get(args.parentMessageId!)
+    if (!args.conversationId && !args.channelId && !args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId!);
 
       if (!parentMessage) {
         throw new Error("Parent message not found");
       }
 
-      _conversation_id = parentMessage.conversationId
+      _conversation_id = parentMessage.conversationId;
     }
-
 
     const messageId = await ctx.db.insert("messages", {
       memberId: member._id,
--- convex/schema.ts
+++ convex/schema.ts
@@ -40,17 +40,18 @@ const schema = defineSchema({
     .index("by_member_id", ["memberId"])
     .index("by_channel_id", ["channelId"])
     .index("by_conversation_id", ["conversationId"])
+    .index("by_parent_message_id", ["parentMessageId"])
     .index("by_channel_id_parent_message_id_conversation_id", [
       "channelId",
       "parentMessageId",
       "conversationId",
     ]),
-    reactions: defineTable({
-      workspaceId: v.id("workspaces"),
-      messageId: v.id("messages"),
-      memberId: v.id("members"),
-      value: v.string(),
-    })
+  reactions: defineTable({
+    workspaceId: v.id("workspaces"),
+    messageId: v.id("messages"),
+    memberId: v.id("members"),
+    value: v.string(),
+  })
     .index("by_workspace_id", ["workspaceId"])
     .index("by_message_id", ["messageId"])
     .index("by_member_id", ["memberId"]),
