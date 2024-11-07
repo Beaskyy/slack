bfa0f415a85115b2869ede59c9de59c398c6322b
Beaskyy <afeez.akintobi@yahoo.com>
2024-11-07 21:56:50 +0100

    message list

---
+++ src/components/message-list.tsx
@@ -0,0 +1,31 @@
+import { GetMessagesReturnType } from "@/features/messages/api/use-get-message";
+
+interface MessageListProps {
+  memberName?: string;
+  messageImage?: string;
+  channelName?: string;
+  channelCreationTime?: number;
+  variant?: "channel" | "thread" | "conversation";
+  data: GetMessagesReturnType | undefined;
+  loadMore: () => void;
+  isLoadingMore: boolean;
+  canLoadMore: boolean;
+}
+
+export const MessageList = ({
+  memberName,
+  messageImage,
+  channelName,
+  channelCreationTime,
+  data,
+  variant,
+  loadMore,
+  isLoadingMore,
+  canLoadMore,
+}: MessageListProps) => {
+  return (
+    <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
+      MessageList
+    </div>
+  );
+};
